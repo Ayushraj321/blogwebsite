6263266ac900367cd72ba88667f873da64f20844
Ayush Raj <ayushraj2881@gmail.com>
2025-08-19 15:12:15 +0530

    Configure for Vercel deployment

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', Router);
+app.use('/api', Router);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('../client/build'));
